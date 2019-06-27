1cbda74af2ae12d4879efe126d169d3c5228ed9e
pranathiboddu <pranathiboddu37@gmail.com>
2019-06-27 10:13:25 +0530

    javaprogramsPE1 added

--- src/TomandJerry.java
+++ src/TomandJerry.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Evenodd {
+public class TomandJerry {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int number=sc.nextInt(); //intializing and reading the number
